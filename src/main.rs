9d3dc2ef55e8032af477ce67ce41b548f86b8758
bobcats <brian@madsalty.com>
2018-01-22 19:11:20 -0800

    Add Clap for cli arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate clap;
 extern crate plist;
 #[macro_use]
 extern crate serde_derive;
@@ -5,16 +6,25 @@ extern crate serde_ini;
 
 use plist::serde::deserialize;
 use std::fs::File;
+use clap::{App, Arg};
 
 mod iterm2;
 mod mintty;
 
 fn main() {
-    let file = File::open("./tests/data/Hybrid.itermcolors").unwrap();
+    let matches = App::new("iterm2mintty")
+        .version("1.0")
+        .about("Converts iterm2 themes to mintty format")
+        .author("bobcats")
+        .arg(
+            Arg::with_name("iterm2theme").index(1).required(true)
+        )
+        .get_matches();
+
+    let file_path = matches.value_of("iterm2theme").unwrap();
+    let file = File::open(file_path).unwrap();
     let theme: iterm2::Theme = deserialize(file).unwrap();
-    println!("{:?}", &theme);
     let mintty_theme: mintty::Theme = mintty::Theme::from(theme);
-    println!("{:?}", &mintty_theme);
     let ini = serde_ini::to_string(&mintty_theme).unwrap();
     println!("{}", &ini);
 }
