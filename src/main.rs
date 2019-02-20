61ca520f4b9a8263eacf3840492e9a0a2f11f853
Brian Field <brian@madsalty.com>
2019-02-20 10:37:57 -0800

    Add alacritty support, bump version to 0.1.2

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate plist;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_ini;
+extern crate serde_yaml;
 
+use clap::{App, Arg};
 use plist::serde::deserialize;
 use std::fs::File;
-use clap::{App, Arg};
 
+mod alacritty;
 mod iterm2;
 mod mintty;
 
@@ -22,16 +24,26 @@ const DESERIALIZE_ERROR_MSG: &str = r#"""
 
 fn main() {
     let matches = App::new("iterm2mintty")
-        .version("0.1.1")
+        .version("0.1.2")
         .about("Converts iterm2 themes to mintty format")
         .author("bobcats")
         .arg(Arg::with_name("iterm2theme").index(1).required(true))
+        .arg(Arg::with_name("alacritty").short("a").help("Sets destination theme format to alacritty format"))
         .get_matches();
 
     let file_path = matches.value_of("iterm2theme").unwrap();
     let file = File::open(file_path).expect(FILE_NOT_FOUND_ERROR_MSG);
+    let alacritty = matches.is_present("alacritty");
     let theme: iterm2::Theme = deserialize(file).expect(DESERIALIZE_ERROR_MSG);
-    let mintty_theme: mintty::Theme = mintty::Theme::from(theme);
-    let ini = serde_ini::to_string(&mintty_theme).unwrap();
-    println!("{}", &ini);
+    let output = if alacritty {
+        let alacritty_theme: alacritty::Theme = alacritty::Theme::from(theme);
+
+        serde_yaml::to_string(&alacritty_theme).unwrap()
+    } else {
+        let mintty_theme: mintty::Theme = mintty::Theme::from(theme);
+
+        serde_ini::to_string(&mintty_theme).unwrap()
+    };
+
+    println!("{}", &output);
 }
